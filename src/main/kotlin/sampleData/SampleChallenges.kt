c8a32317f7ec4f38249444c776541080c4717d44
Sam Sauder <sauders@wwu.edu>
2025-09-15 20:35:55 -0700

    Added new Visualization, Tactics, Rapid, and Guess challenges.

--- src/main/kotlin/sampleData/SampleChallenges.kt
+++ src/main/kotlin/sampleData/SampleChallenges.kt
@@ -5,19 +5,44 @@ import model.Challenge
 
 
 // Sam
-val newV7 = Challenge("newV7", limit, Descriptions.NEW_V7, 15, 0.25)
-val projectV7 = Challenge("projectV7", limit, Descriptions.PROJECT_V7, 15, 0.60)
-val projectV8 = Challenge("projectV8", limit, Descriptions.PROJECT_V7, 45, 0.05)
-val threeV5nineV6 = Challenge("threeV5nineV6", volume, Descriptions.THREE_V5_NINE_V6, 90, 0.10)
-
+// Bouldering
+val newV7 = Challenge("newV7", limit, Descriptions.NEW_V7, 15.0, 0.25)
+val projectV7 = Challenge("projectV7", limit, Descriptions.PROJECT_V7, 15.0, 0.60)
+val projectV8 = Challenge("projectV8", limit, Descriptions.PROJECT_V7, 45.0, 0.05)
+val threeV5nineV6 = Challenge("threeV5nineV6", volume, Descriptions.THREE_V5_NINE_V6, 90.0, 0.10)
 val boulderingPlan1Challenges = mutableListOf(projectV8, newV7)
 val boulderingPlan2Challenges = mutableListOf(threeV5nineV6)
 
+// Visualization
+val colorRecall = Challenge("colorRecall", visualization, Descriptions.COLOR_RECALL, 0.10, 0.50)
+val staticRecall70 = Challenge("staticRecall70", visualization, Descriptions.STATIC_RECALL_70, 1.0, 0.20)
+val staticRecall80 = Challenge("staticRecall80", visualization, Descriptions.STATIC_RECALL_80, 1.0, 0.15)
+val staticRecall90 = Challenge("staticRecall90", visualization, Descriptions.STATIC_RECALL_90, 1.0, 0.10)
+val staticRecall100 = Challenge("staticRecall100", visualization, Descriptions.STATIC_RECALL_100, 1.0, 0.05)
+val chessViChallenges = mutableListOf(colorRecall, staticRecall70, staticRecall80, staticRecall90, staticRecall100)
+
+// Tactics
+val puzzle1m2100 = Challenge("puzzle1m2100", tactics, Descriptions.PUZZLE_1M_2100, 1.0, 0.92)
+val puzzle1m2200 = Challenge("puzzle1m2200", tactics, Descriptions.PUZZLE_1M_2200, 1.0, 0.50)
+val puzzle1m2300 = Challenge("puzzle1m2300", tactics, Descriptions.PUZZLE_1M_2300, 1.0, 0.33)
+val puzzle1m2500 = Challenge("puzzle1m2500", tactics, Descriptions.PUZZLE_1M_2500, 1.0, 0.07)
+val chessTaChallenges = mutableListOf(puzzle1m2100, puzzle1m2200, puzzle1m2300, puzzle1m2500)
+
+// Rapid
+val rapid2000 = Challenge("rapid2000", rapid, Descriptions.RAPID_2000, 30.0, 0.50)
+val rapid2100 = Challenge("rapid2100", rapid, Descriptions.RAPID_2100, 30.0, 0.40)
+val rapid2200 = Challenge("rapid2200", rapid, Descriptions.RAPID_2200, 30.0, 0.25)
+val rapid2300 = Challenge("rapid2300", rapid, Descriptions.RAPID_2300, 30.0, 0.10)
+val chessRaChallenges = mutableListOf(rapid2000, rapid2100, rapid2200, rapid2300)
+
+// Guess
+val guess8pt6 = Challenge("guess8pt6", guess, Descriptions.GUESS_8PT6, 30.0, 0.25)
+val chessGuChallenges = mutableListOf(guess8pt6)
+
 
 // Will
-val fiveForty = Challenge("fiveForty", karate, Descriptions.FIVE_FORTY, 1, 0.80)
-val reverseCrescent = Challenge("reverseCrescent", karate, Descriptions.REVERSE_CRESCENT, 1, 0.80)
-val tornado = Challenge("tornado", karate, Descriptions.TORNADO, 1, 0.60)
-val spinningTornado = Challenge("spinningTornado",karate, Descriptions.SPINNING_TORNADO, 1, 0.30)
-
+val fiveForty = Challenge("fiveForty", karate, Descriptions.FIVE_FORTY, 1.0, 0.80)
+val reverseCrescent = Challenge("reverseCrescent", karate, Descriptions.REVERSE_CRESCENT, 1.0, 0.80)
+val tornado = Challenge("tornado", karate, Descriptions.TORNADO, 1.0, 0.60)
+val spinningTornado = Challenge("spinningTornado",karate, Descriptions.SPINNING_TORNADO, 1.0, 0.30)
 val karateChallenges = mutableListOf(fiveForty, reverseCrescent, tornado, spinningTornado)
