28428f2636df09ca71b19cbe078e0d774e5b60b9
Sam Sauder <sauders@wwu.edu>
2025-09-15 20:35:07 -0700

    Added the chess7D non-day plan.

--- src/main/kotlin/sampleData/SampleNonDayPlans.kt
+++ src/main/kotlin/sampleData/SampleNonDayPlans.kt
@@ -3,7 +3,7 @@ package sampleData
 import model.NonDayPlan
 import model.auxiliary.Type
 
-val boulderingWeekPlan1 = NonDayPlan("boulderingWeekPlan", Type.week, bouldering,
-    mutableListOf(limit, volume, hang, pull, core, antagonist),
-    "2025-09-01"
-)
+val boulderingWeekPlan1 = NonDayPlan("boulderingWeekPlan",
+    Type.week, bouldering, bouldering.subskills,"2025-09-01")
+
+val chess7D = NonDayPlan("chess7D", Type.week, chess, chess.subskills, "2025-10-01")
