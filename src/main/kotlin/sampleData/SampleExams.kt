613cee3cfc808c98f7960a703cea3f2dc5184a44
Sam Sauder <sauders@wwu.edu>
2025-09-17 07:11:14 -0700

    Renamed Test to Exam.

---
+++ src/main/kotlin/sampleData/SampleExams.kt
@@ -0,0 +1,17 @@
+package sampleData
+
+import constants.Descriptions
+import model.Exam
+
+// Sam
+val pullup2RMplus50lb = Exam("plus50pullup2RM", pull, Descriptions.PULLUP_2RM_PLUS_50LB , 1.0)
+val bench1RM80lb = Exam("bench1RM80lb", antagonist, Descriptions.BENCH_1RM_80LB , 1.0)
+
+val boulderingPlan1Tests = mutableListOf(pullup2RMplus50lb)
+val boulderingPlan2Tests = mutableListOf(bench1RM80lb)
+
+
+// Will
+val yellowBeltExam = Exam("yellowBeltTest", karate, Descriptions.YELLOW_BELT_TEST, 30.0)
+
+val karateTests = mutableListOf(yellowBeltExam)
