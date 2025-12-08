e8fd5e51cd6176d11976a70912f17d8c30c34b81
Sam Sauder <sauders@wwu.edu>
2025-12-08 02:24:16 -0800

    Started the repl with FlowAlt

--- src/main/kotlin/com/idris/Main.kt
+++ src/main/kotlin/com/idris/Main.kt
@@ -2,7 +2,11 @@ package com.idris  // Main
 
 
 fun main(args: Array<String>) {
-    val f = Flow()
+    // val f = Flow()
     // f.begin()  // Start prompting the user for commands
-    f.beginAlt(args);
+    // f.beginAlt(args);
+
+    val fa = FlowAlt()
+    // fa.begin(args)
+    fa.repl("../../testdata/testData.db")
 }
