1f23ee24653fdef2c750703e60a4fdd4041d225c
Sam Sauder <sauders@wwu.edu>
2025-12-08 02:18:40 -0800

    Implemented CreateHelper
    Functions
    f: add a new FoundationE to the database
    c: add a new ChallengeE to the database
    e: add a new ExamE to the database

---
+++ src/main/kotlin/com/idris/database/CreateHelper.kt
@@ -0,0 +1,85 @@
+package com.idris.database
+
+import com.idris.model.Exam
+import com.idris.model.Foundation
+import com.idris.model.challenge.Challenge
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import java.util.Scanner
+
+// Helper functions for the 'create' Idris command
+
+
+object CreateHelper : Helper() {
+    // ======================================================================
+    override fun f() {  // add a FoundationE to the database
+        val f = Foundation(
+            "",
+            null,
+            "",
+            -1.0)
+        this.fillObjectiveCore(f)
+        val skillName = f.skill?.id
+
+        transaction {
+            FoundationE.new {
+                name = f.name
+                if (skillName != null) skill = skillName;
+                description = f.description
+                minutes = f.minutes.toBigDecimal()
+            }
+        }
+
+        println("\nAdded '${f.name}' to the Foundation table.")
+    }
+    // ======================================================================
+    override fun c() {  // add a ChallengeE to the database
+        val c = Challenge("", null, "", -1.0, 0.01)
+        this.fillObjectiveCore(c)
+        val skillName = c.skill?.id
+
+        val s = Scanner(System.`in`)
+        print("ODDS  ")
+        c.userOdds = s.nextDouble()
+        c.generateChallengeElo()
+
+        transaction {
+            ChallengeE.new {
+                name = c.name
+                // skill = (c.skill?.id ?: String) as String
+                if (skillName != null) skill = skillName;
+                description = c.description
+                minutes = c.minutes.toBigDecimal()
+                cElo = c.challengeElo.toBigDecimal()
+                uElo = c.userElo.toBigDecimal()
+                uOdds = c.userOdds.toBigDecimal()
+            }
+        }
+
+        println("\nAdded '${c.name}' to the Challenge table.")
+    }
+    // ======================================================================
+    override fun e() {  // add an ExamE to the database
+        val e  = Exam(
+            "",
+            null,
+            "",
+            -1.0,
+            false)
+        this.fillObjectiveCore(e)
+        val skillName = e.skill?.id
+
+        transaction {
+            ExamE.new {
+                name = e.name
+                if (skillName != null) skill = skillName;
+                description = e.description
+
+                minutes = e.minutes.toBigDecimal()
+                passed = false
+            }
+        }
+
+        println("\nAdded '${e.name}' to the Exam table.")
+    }
+    // ======================================================================
+}
