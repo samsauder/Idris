7331572ac513d3a93489060eb89da2a0a4261cff
Sam Sauder <sauders@wwu.edu>
2025-12-08 02:15:55 -0800

    Created DeleteHelper skeleton

---
+++ src/main/kotlin/com/idris/database/DeleteHelper.kt
@@ -0,0 +1,18 @@
+package com.idris.database
+
+// Helper functions for the 'delete' Idris command
+
+
+object DeleteHelper : Helper() {
+    override fun f() {
+        TODO("Not yet implemented")
+    }
+
+    override fun c() {
+        TODO("Not yet implemented")
+    }
+
+    override fun e() {
+        TODO("Not yet implemented")
+    }
+}
