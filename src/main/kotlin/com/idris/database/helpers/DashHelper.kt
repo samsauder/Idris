07fe447c6c8ddb964f1dac1fbf4ab6f15b480aab
Sam Sauder <sauders@wwu.edu>
2025-12-10 09:50:51 -0800

    Added skeleton for DashHelper

---
+++ src/main/kotlin/com/idris/database/helpers/DashHelper.kt
@@ -0,0 +1,23 @@
+package com.idris.database.helpers
+
+// Call the Idris 'dash' operation for Foundation, Challenge, Exam, or Experiment
+// - dash brings up a detailed overview of the specified Idris entity
+
+
+object DashHelper : Helper() {
+    override fun f() {
+        TODO("Not yet implemented")
+    }
+
+    override fun c() {
+        TODO("Not yet implemented")
+    }
+
+    override fun e() {
+        TODO("Not yet implemented")
+    }
+
+    override fun x() {
+        TODO("Not yet implemented")
+    }
+}
