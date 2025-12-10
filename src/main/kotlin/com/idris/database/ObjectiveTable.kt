e50641ef82b58dcaf391f05fd95810089c621625
Sam Sauder <sauders@wwu.edu>
2025-12-10 10:24:04 -0800

    Defined common Objective attributes in ObjectiveTable

---
+++ src/main/kotlin/com/idris/database/ObjectiveTable.kt
@@ -0,0 +1,12 @@
+package com.idris.database
+
+import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
+
+// Defines common attributes for all Idris Objective tables
+
+abstract class ObjectiveTable(tableName: String) : IntIdTable(tableName) {
+    val name = varchar("name", 50)
+    val skill = varchar("skill", 50)
+    val description = varchar("description", 200)
+    val minutes = decimal("minutes",5, 2)
+}
