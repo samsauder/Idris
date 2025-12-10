711d97d056ae2ed8791d801cd03d37519acd7c19
Sam Sauder <sauders@wwu.edu>
2025-12-10 09:51:53 -0800

    Changed path of .db file
    Since the Makefile is in the root directory now

--- src/main/kotlin/com/idris/Main.kt
+++ src/main/kotlin/com/idris/Main.kt
@@ -4,7 +4,7 @@ package com.idris  // Main
 fun main(args: Array<String>) {
     val fa = FlowAlt()
 
-    fa.repl("../../sdata/realData.db")
+    fa.repl("sdata/realData.db")
     // fa.repl("../../sdata/realData.db")
     // fa.repl("../../testdata/testData.db")
 }
