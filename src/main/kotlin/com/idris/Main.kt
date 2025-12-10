990b33909b1ef91ddab6700210273eb8c3d36744
Sam Sauder <sauders@wwu.edu>
2025-12-10 06:52:42 -0800

    Switched to new real database

--- src/main/kotlin/com/idris/Main.kt
+++ src/main/kotlin/com/idris/Main.kt
@@ -4,6 +4,7 @@ package com.idris  // Main
 fun main(args: Array<String>) {
     val fa = FlowAlt()
 
+    fa.repl("../../sdata/realData.db")
     // fa.repl("../../sdata/realData.db")
-    fa.repl("../../testdata/testData.db")
+    // fa.repl("../../testdata/testData.db")
 }
