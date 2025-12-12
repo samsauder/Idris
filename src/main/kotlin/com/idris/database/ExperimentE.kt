84e6adcd4aad157665124f6473b75a261c0d72c2
Sam Sauder <sauders@wwu.edu>
2025-12-11 23:54:40 -0800

    Added some attributes to ExperimentsT

--- src/main/kotlin/com/idris/database/ExperimentE.kt
+++ src/main/kotlin/com/idris/database/ExperimentE.kt
@@ -5,7 +5,10 @@ import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
 import org.jetbrains.exposed.v1.dao.IntEntity
 
 object ExperimentsT : IntIdTable("experimentsT") {
-
+    val name = varchar("name", 50)
+    val description = varchar("description", 800)
+    // val segmentName = varchar("segmentName", 50)
+    // var segCount = integer("segCount")
 }
 
 class ExperimentE(id: EntityID<Int>) : IntEntity(id) {
