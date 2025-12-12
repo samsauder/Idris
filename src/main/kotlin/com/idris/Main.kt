4edfb1828c1cb6445d870c3e07a50b5bc61f268a
Sam Sauder <sauders@wwu.edu>
2025-12-12 05:30:58 -0800

    Formatting

--- src/main/kotlin/com/idris/Main.kt
+++ src/main/kotlin/com/idris/Main.kt
@@ -1,10 +1,10 @@
-package com.idris  // Main
+package com.idris
+
+// Main
 
 
 fun main(args: Array<String>) {
     val fa = FlowAlt()
 
     fa.repl("sdata/realData.db")
-    // fa.repl("../../sdata/realData.db")
-    // fa.repl("../../testdata/testData.db")
 }
