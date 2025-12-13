b650d4344a79e1563a5ada00462753ec63b5d4dd
Sam Sauder <sauders@wwu.edu>
2025-12-12 23:26:52 -0800

    Put old App database function into Database

--- src/main/kotlin/com/idris/Database.kt
+++ src/main/kotlin/com/idris/Database.kt
@@ -1,6 +1,14 @@
 package com.idris
+import com.idris.database.ChAttemptsT
+import com.idris.database.ChallengesT
+import com.idris.database.ExamsT
+import com.idris.database.FoundationsT
+import com.idris.database.ProgressionsT
 import org.jetbrains.exposed.v1.jdbc.Database
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.deleteAll
 import org.jetbrains.exposed.v1.jdbc.transactions.TransactionManager
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import java.sql.Connection
 
 // An abstraction for an SQLITE database of Idris entities on the host machine
@@ -18,5 +26,32 @@ class Database(var path: String) {  // specify a valid .db file with path
         TransactionManager.manager.defaultIsolationLevel =
             Connection.TRANSACTION_SERIALIZABLE
 
+    }
+
+    // Set up the tables for the current database
+    fun reset(deleteEntities: Boolean) {
+        transaction {
+            if (deleteEntities) deleteEntities()  // remove all entities
+
+            SchemaUtils.create(ChallengesT)
+            SchemaUtils.create(ChAttemptsT)
+            SchemaUtils.create(FoundationsT)
+            SchemaUtils.create(ExamsT)
+            SchemaUtils.create(ProgressionsT)
+            // SchemaUtils.create(ExperimentsT)
+            // SchemaUtils.create(DaysT)
+
+            println("Set up all tables for the database at '$path'.\n")
+        }
+    }
+
+    // Delete all entities in the database
+    private fun deleteEntities() {
+        transaction {
+            ChallengesT.deleteAll()
+            ChAttemptsT.deleteAll()
+            FoundationsT.deleteAll()
+            ExamsT.deleteAll()
+        }
     }
 }
