40d05f021f12bd60396644907bf03f8b35e3dec6
Sam Sauder <sauders@wwu.edu>
2025-12-12 23:31:45 -0800

    Integrated massLog into the Challenge log operation

--- src/main/kotlin/com/idris/database/helpers/LogHelper.kt
+++ src/main/kotlin/com/idris/database/helpers/LogHelper.kt
@@ -2,6 +2,8 @@ package com.idris.database.helpers
 
 import com.idris.database.ChallengeE
 import com.idris.database.ChallengesT
+import com.idris.database.ProgressionE
+import com.idris.database.ProgressionsT
 import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import java.math.BigDecimal
@@ -17,30 +19,22 @@ object LogHelper : Helper() {
         }
     }
     // ======================================================================
-    override fun c() {
+    override fun c(datapath: String) {
         transaction {
             val name = inputName()
             val resultString = inputResult()
             println()
 
-            // val cIterator = ChallengeE.Companion.find { ChallengesT.name eq name }.iterator()
-            // val cE = cIterator.next()
-            // val c = cE.deEntify()
+            val cIterator = ChallengeE.Companion.find { ChallengesT.name eq name }.iterator()
+            val cE = cIterator.next()
+            val c = cE.deEntify()
+            val pIterator = ProgressionE.Companion.find { ProgressionsT.name eq c.progressionName }.iterator()
+            val pE = pIterator.next()
+            val p = pE.deEntify(datapath)
+
             val result = if (resultString == "win") 1.0 else 0.0
-            cManual(name, result)
-            /*
-            c.log(resultDouble)
-
-            val challengeE = ChallengeE.Companion.findSingleByAndUpdate(ChallengesT.name eq name) {
-                it.cElo = BigDecimal.valueOf(c.challengeElo)
-                it.uElo = BigDecimal.valueOf(c.userElo)
-                it.uOdds = BigDecimal.valueOf(c.userOdds)
-                it.attempts++
-                it.wins += resultDouble.toInt()
-            }
-
-            // add an entry to the challenge attempts table
-            */
+            p.massLog(c.name, result)
+            // cManual(name, result)
         }
     }
 
