70d3419a1279c4c61ca7ee4804b055e0be68609a
Sam Sauder <sauders@wwu.edu>
2025-12-15 04:29:37 -0800

    Created the ConceptType enum
    FOUNDATION
    CHALLENGE
    EXAM
    PROGRESSION
    DAY
    EXPERIMENT

---
+++ src/main/kotlin/com/idris/model/auxiliary/ConceptType.kt
@@ -0,0 +1,9 @@
+package com.idris.model.auxiliary
+
+// The IdrisEntity enum categorizes everything in the database that has its own table
+
+
+enum class ConceptType {
+    FOUNDATION, CHALLENGE, EXAM,   // Objectives
+    PROGRESSION, DAY, EXPERIMENT   // Other
+}
