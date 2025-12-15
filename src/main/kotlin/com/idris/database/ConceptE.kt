bc1c81dfcd8aeb44c4647eb852c310d0a15b1a96
Sam Sauder <sauders@wwu.edu>
2025-12-15 04:23:45 -0800

    ConceptE abstract class (Concept entity)
    Every class of superclass ConceptE must have a deEntify() function

---
+++ src/main/kotlin/com/idris/database/ConceptE.kt
@@ -0,0 +1,12 @@
+package com.idris.database
+
+import com.idris.model.newclasses.Concept
+import org.jetbrains.exposed.v1.core.dao.id.EntityID
+import org.jetbrains.exposed.v1.dao.IntEntity
+
+// Abstract class defining common attributes for all Idris database entities
+
+
+abstract class ConceptE(id: EntityID<Int>) : IntEntity(id) {
+    abstract fun deEntify() : Concept;  // return a Concept
+}
