2a104c76c98bef1297d461d31de4cd4999cc36f8
Sam Sauder <sauders@wwu.edu>
2025-12-19 01:45:23 -0800

    Main changes

--- src/main/kotlin/com/idris/Main.kt
+++ src/main/kotlin/com/idris/Main.kt
@@ -6,13 +6,14 @@ package com.idris
 fun main(args: Array<String>) {
     // val path = "sdata/real.db"
     val path = "testdata/testData.db"
-    reload(path, false)
+    // val path = "testdata/willData.db"
+    // reload(path, false)
     FlowAlt.repl(path)
 }
 
 
 // Reloads the specified database (reconnects and resets)
-fun reload(path: String, doReset: Boolean) {
+fun reload(path: String, deleteEntities: Boolean) {
     val database = Database(path)
-    database.reset(doReset)
+    database.reset(deleteEntities)
 }
