94f31a3a120fce67a3861b6c63d0019f1790abc3
Sam Sauder <sauders@wwu.edu>
2026-01-05 17:05:45 -0800

    Refactored the Concept class
    - style and padding are now handled by the format function
    - added print (comprehensive) and printL (one-line)
    - added the icon abstract value

--- src/main/kotlin/com/idris/model/newclasses/Concept.kt
+++ src/main/kotlin/com/idris/model/newclasses/Concept.kt
@@ -1,5 +1,7 @@
 package com.idris.model.newclasses
 
+import com.idris.constants.Styles
+
 // The Concept is the superclass of all Idris classes and stores common attributes:
 // + name        |  a unique identifier
 // + description |  a brief (< 200 char) synopsis
@@ -12,4 +14,43 @@ package com.idris.model.newclasses
 abstract class Concept {
     abstract val name: String
     abstract val description: String
+    abstract val icon: String  // one character visual marker
+
+    init {
+        // nameStr = pad(name,  20)
+    }
+
+    // Returns a string composed of this Concept's icon and its formatted name
+    fun icon_name(): String {
+        val fname = format(name, null, 20)  // formatted name
+        return "[$icon] $fname"
+    }
+
+    abstract fun print()   // print a comprehensive representation
+    abstract fun printL()  // print a one-line representation
+
+    /*
+    protected fun style(s: String, style: String) : String{
+        return "$style$s${Styles.RESET}"
+    }
+
+    // Returns a whitespace-padded version of the given string
+    protected fun pad(s: String, padLen: Int) : String {
+        return s.padEnd(padLen, ' ')
+    }
+
+    // Returns an ansi-styled and whitespace-padded version of the given string
+    protected fun styleAndPad(s: String, style: String?, padLen: Int) : String {
+        return "$style${s.padEnd(padLen, ' ')}${Styles.RESET}"
+    }
+    */
+
+    // Format a string to a specific style and pad it with whitespace to meet the specified width
+    // * input null for style and/or padding if desired
+    private fun format(s: String, style: Styles?, width: Int?): String {
+        val middle = if (width != null) str.padEnd(width, ' ') else s
+        val fin = if (style != null) Styles.RESET else ""  // reset style at end if style was applied
+        return "$style$middle$fin"
+    }
+
 }
