f9afa512277985df0001ed7f5aaf0695f909031a
Sam Sauder <sauders@wwu.edu>
2026-01-05 19:59:27 -0800

    Switched to 0551.db test file

--- src/main/kotlin/com/idris/Main.kt
+++ src/main/kotlin/com/idris/Main.kt
@@ -2,6 +2,7 @@ package com.idris
 
 // Main
 fun main(args: Array<String>) {
-    val path = "testdata/055.db"
+    // val path = "testdata/055.db"
+    val path = "testdata/0551.db"
     FlowAlt.repl(path)
 }
