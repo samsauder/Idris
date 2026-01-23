44fe05bca43f67d7d216f4b9152bdd0c44353c57
Sam Sauder <samsauder0@gmail.com>
2026-01-23 03:18:18 -0800

    Refactored Lister
    Condensed logic into list(ConceptType) function.

--- src/main/kotlin/com/idris/database/operators/Lister.kt
+++ src/main/kotlin/com/idris/database/operators/Lister.kt
@@ -1,90 +1,73 @@
 package com.idris.database.operators
 
 import com.idris.database.entities.CHALLENGE
+import com.idris.database.entities.CONCEPT
 import com.idris.database.entities.DAY
 import com.idris.database.entities.EXAM
 import com.idris.database.entities.EXPERIMENT
 import com.idris.database.entities.FOUNDATION
 import com.idris.database.entities.PROGRESSION
+import com.idris.system.extra.ConceptType
+import org.jetbrains.exposed.v1.jdbc.SizedIterable
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 
 // Call the Idris 'list' operation for Foundation, Challenge, or Exam
 
 
 object Lister : Operator() {
-    // fun list(ct: ConceptType) {}
+    // List all concepts of the given concept type (with a certain width table)
+    fun list(ct: ConceptType, width: Int) {
+        transaction {
+            println("${ct}S")
+            println(bar("=",width))
+            for (E in getAll(ct)!!) {  // for each entity of type ct
+                val e = E.deEntify()
+                e.printL()
+            }
+        }
+    }
+
+    // Get all entities of a given concept type
+    fun getAll(ct: ConceptType): SizedIterable<CONCEPT>? {
+        var i: SizedIterable<CONCEPT>? = null
+        transaction {
+            i = when (ct) {
+                ConceptType.FOUNDATION -> {FOUNDATION.Companion.all()}
+                ConceptType.CHALLENGE -> {CHALLENGE.Companion.all()}
+                ConceptType.EXAM -> {EXAM.Companion.all()}
+                ConceptType.DAY -> {DAY.Companion.all()}
+                ConceptType.PROGRESSION -> {PROGRESSION.Companion.all()}
+                ConceptType.EXPERIMENT -> {EXPERIMENT.Companion.all()}
+                // ConceptType.RECORD -> {}
+            }
+        }
+        return i
+    }
 
     // ======================================================================
     override fun f() {
-        transaction {
-            println("FOUNDATIONS")
-            println(bar)
-            for (foundationEntity in FOUNDATION.Companion.all()) {
-                val foundation = foundationEntity.deEntify()
-                foundation.printL()
-            }
-        }
+        list(ConceptType.FOUNDATION, 100)
     }
     // ======================================================================
     override fun c(datapath: String) {
-        transaction {
-            println("CHALLENGES")
-            println(barc)
-            for (challengeEntity in CHALLENGE.Companion.all()) {
-                val challenge = challengeEntity.deEntify()
-                challenge.printL()
-            }
-        }
+        list(ConceptType.CHALLENGE, 100)
     }
     // ======================================================================
     override fun e() {
-        transaction {
-            println("EXAMS")
-            println(bar)
-            for (examEntity in EXAM.Companion.all()) {
-                val exam = examEntity.deEntify();
-                exam.printL()
-            }
-        }
+        list(ConceptType.EXAM, 100)
     }
     // ======================================================================
     override fun x() {
-        // TODO("Not yet implemented")
-        println("EXPERIMENTS")
-        println(barc)
-        transaction {
-            for (expEntity in EXPERIMENT.Companion.all()) {
-                val day = expEntity.deEntify();
-                day.printL()
-            }
-        }
+        list(ConceptType.EXPERIMENT, 100)
     }
     // ======================================================================
     override fun d() {
-        // TODO("Not yet implemented")
-        println("DAYS")
-        println(barc)
-        transaction {
-            for (dayEntity in DAY.Companion.all()) {
-                val day = dayEntity.deEntify();
-                day.printL()
-            }
-        }
+        list(ConceptType.DAY, 100)
     }
     // ======================================================================
     override fun p(datapath: String) {
-        println("PROGRESSIONS")
-        println(barc)
-        transaction {
-            for (progressionEntity in PROGRESSION.Companion.all()) {
-                // val progression = progressionEntity.deEntify(datapath);
-                val progression = progressionEntity.deEntify();
-                progression.printL()
-            }
-        }
+        list(ConceptType.PROGRESSION, 100)
     }
     // ======================================================================
-
-
 
 }
