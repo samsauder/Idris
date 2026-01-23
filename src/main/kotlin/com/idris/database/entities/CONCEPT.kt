90181ea550128dc71105024bcf1d72c7fca6f248
Sam Sauder <samsauder0@gmail.com>
2026-01-23 04:40:31 -0800

    Added companion(ConceptType) and getAll(ConceptType) to CONCEPT

--- src/main/kotlin/com/idris/database/entities/CONCEPT.kt
+++ src/main/kotlin/com/idris/database/entities/CONCEPT.kt
@@ -1,18 +1,35 @@
 package com.idris.database.entities
 
 import com.idris.system.concepts.Concept
+import com.idris.system.extra.ConceptType
 import org.jetbrains.exposed.v1.core.dao.id.EntityID
-import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
 import org.jetbrains.exposed.v1.dao.IntEntity
+import org.jetbrains.exposed.v1.dao.IntEntityClass
+import org.jetbrains.exposed.v1.jdbc.SizedIterable
 
 // Abstract class defining common attributes for all Idris database entities
 
-/*
-object CONCEPTS(tableName: String) : IntIdTable(tableName) {
-
-}
-*/
 
 abstract class CONCEPT(id: EntityID<Int>) : IntEntity(id) {
+    companion object {
+        // Return the companion object associated with the given concept type
+        fun companion(t: ConceptType): IntEntityClass<CONCEPT> {
+            return when (t) {
+                ConceptType.FOUNDATION -> { FOUNDATION.Companion }
+                ConceptType.CHALLENGE -> { CHALLENGE.Companion }
+                ConceptType.EXAM -> { EXAM.Companion }
+                ConceptType.DAY -> { DAY.Companion }
+                ConceptType.PROGRESSION -> { PROGRESSION.Companion }
+                ConceptType.EXPERIMENT -> { EXPERIMENT.Companion }
+                // ConceptType.RECORD -> { RECORD.Companion }
+            }
+        }
+
+        // Get all entities of a given concept type
+        fun getAll(t: ConceptType): SizedIterable<CONCEPT> {
+            return CONCEPT.companion(t).all()
+        }
+    }
+
     abstract fun deEntify() : Concept;  // return a Concept
 }
