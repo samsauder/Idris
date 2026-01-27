8fdcb1bba9e95d0f6618d4c48fb58611f1977cb3
Sam Sauder <samsauder0@gmail.com>
2026-01-26 20:04:17 -0800

    Made the Rater abstract class
    Each Rater subclass is responsible for updating the rating of a
    challenge that uses its specific rating system.
    + EloRater
    + Glicko2Rater (todo)
    + ...

---
+++ src/main/kotlin/com/idris/system/extra/Rater.kt
@@ -0,0 +1,12 @@
+package com.idris.system.extra
+
+import com.idris.system.concepts.ChallengeNew
+import com.idris.system.extra.Styler.style
+import java.time.LocalDate
+
+// The Rater is responsible for updating challenge
+
+
+abstract class Rater {
+    abstract fun update(challenge: ChallengeNew, won: Boolean)
+}
