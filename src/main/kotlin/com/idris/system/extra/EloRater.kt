37408f49394a2cea43cc272b439e2b90d2e6f4e5
Sam Sauder <samsauder0@gmail.com>
2026-01-26 20:07:41 -0800

    EloRater uses Elo to rate challenges

---
+++ src/main/kotlin/com/idris/system/extra/EloRater.kt
@@ -0,0 +1,10 @@
+package com.idris.system.extra
+
+import com.idris.system.concepts.Challenge
+import com.idris.system.concepts.ChallengeNew
+
+object EloRater : Rater() {
+    override fun update(challenge: ChallengeNew, won: Boolean) {
+        TODO("Not yet implemented")
+    }
+}
