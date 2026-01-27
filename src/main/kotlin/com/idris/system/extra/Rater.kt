fdc2b7e2b1046b931299947b1f6a2bab717bf619
Sam Sauder <samsauder0@gmail.com>
2026-01-26 20:08:04 -0800

    Small change

--- src/main/kotlin/com/idris/system/extra/Rater.kt
+++ src/main/kotlin/com/idris/system/extra/Rater.kt
@@ -4,7 +4,7 @@ import com.idris.system.concepts.ChallengeNew
 import com.idris.system.extra.Styler.style
 import java.time.LocalDate
 
-// The Rater is responsible for updating challenge
+// The Rater is responsible for updating challenge rating
 
 
 abstract class Rater {
