a706087715b2999ecd6e4dc4e1c65c188366bafc
Sam Sauder <samsauder0@gmail.com>
2026-01-28 04:24:25 -0800

    Completed RECORD
    Added getOneNamed(String) and deEntify()

--- src/main/kotlin/com/idris/database/entities/RECORD.kt
+++ src/main/kotlin/com/idris/database/entities/RECORD.kt
@@ -1,19 +1,23 @@
 package com.idris.database.entities
 
+import com.idris.system.concepts.Record
 import org.jetbrains.exposed.v1.core.dao.id.EntityID
-import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
-import org.jetbrains.exposed.v1.dao.IntEntity
+import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.dao.IntEntityClass
 
 
 object RECORDS : CONCEPTS("recordsT") {
     val objectiveName = varchar("oname", 50)  // objective name (may be challenge, exam, foundation)
     val won = bool("won")
-    val date = varchar("date", 10)  // string representation of the date
+    val date = varchar("date", 30)  // string representation of the date
 }
 
-class RECORD(id: EntityID<Int>) : IntEntity(id) {
-    companion object : IntEntityClass<RECORD>(RECORDS)
+class RECORD(id: EntityID<Int>) : CONCEPT(id) {
+    companion object : IntEntityClass<RECORD>(RECORDS) {
+        fun getOneNamed(name: String): RECORD? {
+            return find { RECORDS.name eq name}.singleOrNull()
+        }
+    }
 
     var name by RECORDS.name
     var skillName by RECORDS.skillName
@@ -22,4 +26,8 @@ class RECORD(id: EntityID<Int>) : IntEntity(id) {
     var objectiveName by RECORDS.objectiveName
     var won by RECORDS.won
     var date by RECORDS.date
+
+    override fun deEntify(): Record {
+        return Record(name, skillName, description, objectiveName, won, date)
+    }
 }
