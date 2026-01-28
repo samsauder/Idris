daf2fb97593618ddf29997e2bd2bf2d05e3861a4
Sam Sauder <samsauder0@gmail.com>
2026-01-28 01:15:21 -0800

    Finished RECORDS

--- src/main/kotlin/com/idris/database/entities/RECORD.kt
+++ src/main/kotlin/com/idris/database/entities/RECORD.kt
@@ -4,11 +4,11 @@ import org.jetbrains.exposed.v1.core.dao.id.EntityID
 import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
 import org.jetbrains.exposed.v1.dao.IntEntity
 import org.jetbrains.exposed.v1.dao.IntEntityClass
-import org.jetbrains.exposed.v1.datetime.*
 
 
 object RECORDS : IntIdTable("chAttemptsT") {
-    val challenge = reference("challenge", CHALLENGES)
+    // val challenge = reference("challenge", CHALLENGES)
+    val oname = varchar("oname", 50)  // objective name (may be challenge, exam, foundation)
     val won = bool("won")
     val date = varchar("date", 10)  // string representation of the date
 }
@@ -16,7 +16,8 @@ object RECORDS : IntIdTable("chAttemptsT") {
 class RECORD(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<RECORD>(RECORDS)
 
-    var challenge by CHALLENGE referencedOn RECORDS.challenge
+    // var challenge by CHALLENGE referencedOn RECORDS.challenge
+    var oname by RECORDS.oname
     var won by RECORDS.won
     var date by RECORDS.date
 }
