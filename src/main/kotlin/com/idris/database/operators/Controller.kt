99c50b27fc89e887d35c14100d8100bfbcb94045
Sam Sauder <samsauder0@gmail.com>
2026-01-30 05:23:16 -0800

    Implemented Controller.log(ObjectiveType)

--- src/main/kotlin/com/idris/database/operators/Controller.kt
+++ src/main/kotlin/com/idris/database/operators/Controller.kt
@@ -1,9 +1,10 @@
 package com.idris.database.operators
 
 import com.idris.database.entities.CHALLENGE
+import com.idris.database.entities.CHALLENGES
 import com.idris.database.entities.CONCEPT
-import com.idris.database.operators.todo.Logger
-import com.idris.database.operators.todo.Operator
+import com.idris.database.entities.PROGRESSION
+import com.idris.database.operators.todo.Creator
 import com.idris.system.concepts.Concept
 import com.idris.system.extra.ConceptState
 import com.idris.system.extra.ConceptType
@@ -13,7 +14,14 @@ import com.idris.system.extra.Styles
 import com.idris.system.extra.Util.bar
 import com.idris.system.extra.Util.getConceptEntity
 import com.idris.system.extra.Util.inputName
+import com.idris.system.extra.Util.inputString
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import java.math.BigDecimal
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
 // The Controller is an abstraction used to operate on database concepts
 // <targets>    f/c/e/p/d/x
@@ -65,7 +73,48 @@ object Controller {
     // ======================================================================
     fun modify() { TODO() }
     // ======================================================================
-    fun log(ot: ObjectiveType) { TODO() }
+
+    @OptIn(ExperimentalTime::class)
+    fun log(ot: ObjectiveType) {
+        transaction {
+            val name = inputName(ConceptType.CHALLENGE, ConceptState.PRESENT)
+            val result = inputString("RESULT  ")
+            val won: Boolean = result == "win"
+
+            when(ot) {
+                ObjectiveType.CHALLENGE -> {  // log challenge
+                    val challenge = CHALLENGE.getOneNamed(name)?.deEntify()
+
+                    if (challenge?.progressionName == "X") {  // challenge has no progression
+                        challenge.log(won)
+                        CHALLENGE.findSingleByAndUpdate(CHALLENGES.name eq name) {
+                            it.cElo = BigDecimal.valueOf(challenge.challengeElo)
+                            it.uElo = BigDecimal.valueOf(challenge.userElo)
+                            it.uOdds = BigDecimal.valueOf(challenge.userOdds)
+                            it.attempts++
+                            it.wins += result.toInt()
+                            it.progressionName = challenge.progressionName
+                        }  // update info
+                        val datetime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).toString()
+                        Creator.r(
+                            "${name}___${datetime}",
+                            challenge.skillName,
+                            "",
+                            name,
+                            won,
+                            datetime)  // add record to the record table
+                    } else {                          // challenge has a progression
+                        val progression = PROGRESSION.getOneNamed(challenge!!.progressionName)?.deEntify()
+                        val resultNum = if (result == "win") 1.0 else 0.0
+                        progression?.massLog(challenge.name, resultNum)
+                    }
+                }
+                ObjectiveType.EXAM -> { TODO() }
+                ObjectiveType.FOUNDATION -> { TODO() }
+            }
+        }
+    }
+
     // ======================================================================
     fun view(t: ConceptType) {
         val name = inputName(t, ConceptState.PRESENT)
