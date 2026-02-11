ac16fe6a7385fe6e256506ee2a0878137deeb6fb
Sam Sauder <samsauder0@gmail.com>
2026-02-11 02:06:08 -0800

    Created conceptCore(Int)
    Prints the information common to all Concept subclasses
    - name
    - skill
    - description
    
    To be used in the print() functions of these subclasses

--- src/main/kotlin/com/idris/system/concepts/Concept.kt
+++ src/main/kotlin/com/idris/system/concepts/Concept.kt
@@ -1,6 +1,8 @@
 package com.idris.system.concepts
 
 import com.idris.system.extra.Styler.format
+import com.idris.system.extra.Styler.pad
+import com.idris.system.extra.Styler.style
 import com.idris.system.extra.Styles
 
 
@@ -30,5 +32,16 @@ abstract class Concept {
         val skillf = format(skillName, Styles.BOLD, 14)
 
         return "$iconf $fname | $skillf"
+    }
+
+    fun conceptCore(labelWidth: Int) {
+        // print(pad(" NAME", labelWidth))
+        // println("  $name")
+
+        print(pad(" SKILL",labelWidth))
+        println(style("  $skillName", Styles.BOLD))
+
+        print(pad(" DESCRIPTION",labelWidth))
+        println("  $description")
     }
 }
