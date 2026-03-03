805bc5c5b21c3b923b1a81b1658f338002127840
Sam Sauder <samsauder0@gmail.com>
2026-03-03 08:09:29 -0800

    Partially implemented isComplete()
    Returns true if every user-editable attribute is non-null for the
    current Concept
    
    Useful to check if the user exited before the concept was full set

--- src/main/kotlin/com/idris/system/concepts/Concept.kt
+++ src/main/kotlin/com/idris/system/concepts/Concept.kt
@@ -1,5 +1,6 @@
 package com.idris.system.concepts
 
+import com.idris.system.extra.ConceptType
 import com.idris.system.extra.Styler.format
 import com.idris.system.extra.Styler.pad
 import com.idris.system.extra.Styler.style
@@ -8,9 +9,10 @@ import com.idris.system.extra.Styles
 
 abstract class Concept {
     abstract val icon: String  // one character visual marker
-    lateinit var name: String
-    lateinit var skillName: String
-    lateinit var description: String
+    abstract val ct: ConceptType
+    var name: String? = null
+    var skillName: String? = null
+    var description: String? = null
 
     constructor(name: String,
                 skillName: String,
@@ -25,11 +27,43 @@ abstract class Concept {
     abstract fun print()   // print a comprehensive representation
     abstract fun printL()  // print a one-line representation
 
+
+    // Return true if every user-editable attribute is non-null for this Concept
+    fun isComplete(): Boolean {
+        // check common concept attributes
+        if (this.name == null) return false
+        if (this.skillName == null) return false
+        if (this.description == null) return false
+
+        if (ct == ConceptType.FOUNDATION ||
+            ct == ConceptType.CHALLENGE ||
+            ct == ConceptType.EXAM) {  // check common objective attributes
+            val o = this as Objective
+            if (o.minutes == null) return false
+        }
+
+        when (ct) {  // check unique attributes
+            ConceptType.FOUNDATION -> {} // nothing extra needed
+            ConceptType.CHALLENGE -> {
+                val ch = this as Challenge
+                if (ch.progressionName == null) return false
+            }
+            ConceptType.EXAM -> {TODO("implement for EXAM")}
+            ConceptType.PROGRESSION -> {TODO("implement for PROGRESSION")}
+            ConceptType.DAY -> {TODO("implement for DAY")}
+            ConceptType.EXPERIMENT -> {TODO("implement for EXPERIMENT")}
+            ConceptType.RECORD -> {TODO("implement for RECORD")}
+        }
+
+        return true
+    }
+
+
     // Return a string composed of this Concept's icon, its formatted name, and its skill
     fun icon_name_skill(): String {
         val iconS = style("$icon", Styles.GREEN)               // icon (styled)
-        val nameS = format(name, "", 20)           // name (styled)
-        val skillS = format(skillName, Styles.BOLD, 14)  // skill (styled)
+        val nameS = format(name!!, "", 20)           // name (styled)
+        val skillS = format(skillName!!, Styles.BOLD, 14)  // skill (styled)
         return "$iconS  $nameS | $skillS"
     }
 
