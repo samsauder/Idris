92f8a4778168f114bae1faf87586669f5475e2a4
Sam Sauder <samsauder0@gmail.com>
2026-03-08 09:54:03 -0700

    Added privilege modes and made exec more modular
    - USER cannot create, delete, or modify the database
    - ADMIN can do every operation
    - exec can now be called using an OperationType and a ConceptType

--- src/main/kotlin/com/idris/Flow.kt
+++ src/main/kotlin/com/idris/Flow.kt
@@ -3,11 +3,18 @@ package com.idris
 import com.idris.database.operators.Controller
 import com.idris.database.operators.todo.Creator
 import com.idris.database.operators.todo.Modifier
+import com.idris.system.concepts.ConceptSymbols
 import com.idris.system.extra.ConceptType
-import com.idris.system.extra.ObjectiveType
+import com.idris.system.extra.OperationType
+import com.idris.system.extra.OperationSymbols
+import com.idris.system.extra.Privilege
 import com.idris.system.extra.Styler.style
 import com.idris.system.extra.Styles
 import com.idris.system.extra.Util
+import com.idris.system.extra.Util.conSymbolOf
+import com.idris.system.extra.Util.opSymbolOf
+import com.idris.system.extra.Util.toConType
+import com.idris.system.extra.Util.toOpType
 
 
 object Flow {
@@ -16,28 +23,42 @@ object Flow {
         val d = Database(path)
         d.connect()
 
-        val version = "0.1"
-        val quote = "The excellence of the soul is understanding; for one who understands is conscious, devoted, and already godlike."
+        val version = "1.0.0"
+        val quote = "The excellence of the soul is understanding\nfor one who understands\nis conscious, devoted, and already godlike."
         Util.idrisBanner()  // print the name Idris (stylized)
         println("Version ${version}\n")
 
         print(style(quote, Styles.YELLOW))
         println("  — Hermes Trismegistus\n\n")
 
-        var input: String? = ""
+        var input: String?
         var inputPrev: String? = ""  // last entered command
 
         while (true) {
-            print(">> ")
+            print("$ ")
             input = readlnOrNull()
 
-            if (input == "q") {  // quit
+            if (input == OperationSymbols.QUIT) {         // QUIT
                 break
-            } else if (input == "") {
-                execCommand(inputPrev)  // execute the most recently executed command
-            } else {
-                execCommand(input)
+            } else if (input == OperationSymbols.HELP) {  // HELP
+                Controller.help()
+                println("\n")
+                continue
+            } else if (input == OperationSymbols.ADMIN) {  // ADMIN
+                Controller.admin()
+                println("\n")
+                continue
+            } else if (input == "") {   // previously entered command
+                input = inputPrev
+            } else {                    // core command
+                val inputIsValid = checkValid(input!!)
+                if (!inputIsValid) continue
             }
+
+            val ot: OperationType = toOpType(opSymbolOf(input!!))!!
+            val ct: ConceptType = toConType(conSymbolOf(input))!!
+
+            exec(ot, ct)
 
             println("\n")
 
@@ -46,56 +67,54 @@ object Flow {
     }
 
 
-    // Execute the given Idris command, return false if the command is invalid
-    fun execCommand(command: String?): Boolean {
-        val args = command?.split(" ")  // parse the args
+    // Executes the passed Idris command and returns true on success
+    fun exec(operation: OperationType, concept: ConceptType): Boolean {
+        val firstChar = concept.toString().substring(0, 1).lowercase()  // workaround until Creator and Modifier are dissolved
 
-        val type = if (args?.size == 2) flagToCT(args[1]) else null
+        when (operation) {
+            OperationType.CREATE -> Creator.choose(firstChar, "")   // TODO switch with Controller equivalent
+            OperationType.MODIFY -> Modifier.choose(firstChar, "")  // TODO switch with Controller equivalent
+            OperationType.DELETE -> Controller.delete(concept)     // DELETE
 
-        if (type == null && args?.size!! > 1 && args[1] != "-t") {  // TODO remove last expression later
-            println("${Util.error()}  invalid flag\n")
-            return false
-        }
-
-        when (args?.get(0)) {  // args: <command> <option>
-            "list" -> Controller.list(type!!)
-            "create" -> Creator.choose(args[1], "")
-            "delete" -> Controller.delete(type!!)
-            "log" -> Controller.log(flagToOT(args[1])!!)
-            "modify" -> Modifier.choose(args[1], "")
-            "view" -> Controller.view(type!!)
-            "help" -> Controller.help()
-
-            else -> {
-                println("${Util.error()}  invalid command\n")
-                return false
-            }
+            OperationType.LOG ->    Controller.log(concept)        // LOG
+            OperationType.LIST ->   Controller.list(concept)       // LIST
+            OperationType.VIEW ->   Controller.view(concept)       // VIEW
+            else -> null
         }
         return true
     }
 
 
-    // Return the ConceptType version of the given CLI flag
-    fun flagToCT(flag: String): ConceptType? {
-        return when (flag) {
-            "-f" -> ConceptType.FOUNDATION
-            "-c" -> ConceptType.CHALLENGE
-            "-e" -> ConceptType.EXAM
-            "-p" -> ConceptType.PROGRESSION
-            "-d" -> ConceptType.DAY
-            "-x" -> ConceptType.EXPERIMENT
-            "-r" -> ConceptType.RECORD
-            else -> {null}  // invalid flag
+    // Return true if the command string is a valid Idris command
+    private fun checkValid(command: String): Boolean {
+        if (command.split(" ").size > 1) {  // check that command is a single word
+            println("${Util.error()}  Command must not have spaces.\n\n")
+            return false
         }
-    }
 
-    // Return the ObjectiveType version of the given CLI flag
-    fun flagToOT(flag: String): ObjectiveType? {
-        return when (flag) {
-            "-f" -> ObjectiveType.FOUNDATION
-            "-c" -> ObjectiveType.CHALLENGE
-            "-e" -> ObjectiveType.EXAM
-            else -> {null}  // invalid flag
+        val o: String = opSymbolOf(command)
+        val c: String = conSymbolOf(command)
+
+        if (Controller.mode == Privilege.USER) {  // check that the operation is authorized for the user
+            val adminOperations = listOf(OperationSymbols.CREATE,
+                                         OperationSymbols.DELETE,
+                                         OperationSymbols.MODIFY)
+
+            if (adminOperations.contains(o)) {
+                println("${Util.error()}  Unauthorized.\n\n")
+                return false
+            }
+        }
+
+        // check that the operation and concept specified exist
+        val opExists = OperationSymbols.all.contains(o)
+        val conExists = ConceptSymbols.all.contains(c)
+
+        if (opExists && conExists) {
+            return true
+        } else {
+            println("${Util.error()}  Invalid command.\n\n")
+            return false
         }
     }
 }
